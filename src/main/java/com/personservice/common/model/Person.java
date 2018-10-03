4966a11fc37ce32db67b74725c4560ec3172692d
Mio Engel <mioengel@gmail.com>
2018-10-03 12:11:25 +0200

    Adds lombok

--- src/main/java/com/personservice/common/model/Person.java
+++ src/main/java/com/personservice/common/model/Person.java
@@ -1,28 +1,11 @@
 package com.personservice.common.model;
 
-public class Person {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class Person {
     private String personNummer;
     private String name;
-
-    public Person(String personNummer, String name) {
-        this.personNummer = personNummer;
-        this.name = name;
-    }
-
-    public String getPersonNummer() {
-        return personNummer;
-    }
-
-    public void setPersonNummer(String personNummer) {
-        this.personNummer = personNummer;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
