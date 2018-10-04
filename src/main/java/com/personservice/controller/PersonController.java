5c84e3295dbdabca5722114e45ffe377b34352be
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-10-04 10:53:27 +0200

    Renames JS to JSON

--- src/main/java/com/personservice/controller/PersonController.java
+++ src/main/java/com/personservice/controller/PersonController.java
@@ -1,6 +1,6 @@
 package com.personservice.controller;
 
-import com.common.util.JS;
+import com.common.util.JSON;
 import com.personservice.common.exception.PersonMissingException;
 import com.personservice.common.model.Person;
 import com.personservice.service.PersonService;
@@ -20,31 +20,31 @@ public class PersonController {
     @GetMapping
     @ResponseBody
     public ResponseEntity<?> getPersons() {
-        return JS.message(HttpStatus.OK, personService.getPersons());
+        return JSON.message(HttpStatus.OK, personService.getPersons());
     }
 
     @GetMapping("/{personnummer}")
     @ResponseBody
     public ResponseEntity<?> getPerson(@PathVariable String personnummer) {
         try {
-            return JS.message(HttpStatus.OK, personService.getPerson(personnummer));
+            return JSON.message(HttpStatus.OK, personService.getPerson(personnummer));
         } catch (PersonMissingException e) {
-            return JS.message(HttpStatus.NOT_FOUND, "Unable to find person with personnnumer: " + personnummer);
+            return JSON.message(HttpStatus.NOT_FOUND, "Unable to find person with personnnumer: " + personnummer);
         }
     }
 
     @PostMapping
     public ResponseEntity<?> createPerson(@RequestBody @Valid Person person) {
-        return JS.message(HttpStatus.OK, personService.createPerson(person));
+        return JSON.message(HttpStatus.OK, personService.createPerson(person));
     }
 
     @DeleteMapping("/{personnummer}")
     public ResponseEntity<?> deletePerson(@PathVariable String personnummer) {
         try {
             personService.deletePerson(personnummer);
-            return JS.message(HttpStatus.OK, "Person with personnummer " + personnummer + " deleted");
+            return JSON.message(HttpStatus.OK, "Person with personnummer " + personnummer + " deleted");
         } catch (PersonMissingException e) {
-            return JS.message(HttpStatus.NOT_FOUND, "Unable to delete person with personnnumer: " + personnummer);
+            return JSON.message(HttpStatus.NOT_FOUND, "Unable to delete person with personnnumer: " + personnummer);
         }
     }
 }
